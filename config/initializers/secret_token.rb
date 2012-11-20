2ec634e92e909a2314d971f8a117a7b25ae3b4c5
Aidan Feldman <aidan.feldman@gmail.com>
2012-11-20 01:57:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoubleAgent::Application.config.secret_token = '0ea8b377bfd917654bd4087da407a638757ecbb67c3f270dbfb809c63fe73f07f0921d1f56cf241a77ae96d391b9a57478f23705f64f331ee907822b54eb3adb'
